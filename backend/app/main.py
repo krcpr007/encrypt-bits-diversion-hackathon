df61b42eabe4135b9828641dd64aab978ddc15e6
Rajan <krcpr007@gmail.com>
2024-02-03 11:54:36 +0000

    Not push bigger files of model

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,10 +3,20 @@ import uvicorn
 from dotenv import load_dotenv
 from routes import route
 from config import database
-
+from fastapi.middleware.cors import CORSMiddleware
 config = load_dotenv(".env")
 
 app = FastAPI(root_path="/api/v1")
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(route.router, dependencies=[Depends(database.get_database)])
 
 app.add_event_handler("startup", database.connect_to_database)
