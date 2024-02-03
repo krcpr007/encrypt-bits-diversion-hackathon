6b2679804f1976653b3d95be07fab04bd71394e2
Rajan <krcpr007@gmail.com>
2024-02-03 08:03:25 +0000

    Feat : s3 object to crud operation for multimedia files (img)

---
+++ backend/app/config/s3.py
@@ -0,0 +1,9 @@
+import boto3
+import os
+
+AWS_ACCESS_KEY = os.getenv['AWS_ACCESS_KEY']
+AWS_SECRET_KEY = os.getenv['AWS_SECRET_KEY']
+S3_BUCKET_NAME = os.getenv['S3_BUCKET_NAME']
+
+# Initialize AWS S3 client
+s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
